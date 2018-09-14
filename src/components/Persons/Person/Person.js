0de7ce550ed14d5e000006ba3379c773d1a14aef
Farooq Qaisrani <farooqqaisrani@outlook.com>
2018-09-14 19:57:50 +0400

    Initial commit starting from Section 7

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './Person.css';
+
+
+const person = (props) => {
+
+    return (
+
+        <div className={classes.Person}>
+            <p onClick={props.click}>This is para {props.name} number {props.age}</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>
+    )
+};
+
+export default person;
---
+++ src/containers/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
+
+class App extends Component {
+  state = {
+    persons: [
+        { id: 'asdafa', name: 'Maximillian', age: 28},
+        { id: 'asdasd', name: 'Manu', age: 29},
+        { id: 'zxczxc', name: 'Stephanie', age: 26}
+    ],
+      showPersons: false
+  }
+
+
+  nameChangedHandler = (event, id) => {
+      const personIndex = this.state.persons.findIndex(p => {
+          return p.id === id;
+      });
+
+      const person = {
+          ...this.state.persons[personIndex]
+      };
+
+      person.name = event.target.value;
+      const persons = [...this.state.persons];
+      persons[personIndex] = person;
+
+      this.setState({persons: persons});
+  }
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
+
+
+    }
+
+    deletePersonHandler = (personIndex) => {
+        // const persons = this.state.persons.slice();
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons})
+
+    }
+
+  render() {
+
+      let persons = null;
+
+      if(this.state.showPersons) {
+          persons = (
+                    <Persons
+                        persons={this.state.persons}
+                        clicked={this.deletePersonHandler}
+                        changed={this.nameChangedHandler}/>
+          );
+
+
+      }
+
+    return (
+      <div className={classes.App}>
+            <Cockpit
+                showpersons={this.state.showPersons}
+                persons={this.state.persons}
+                clicked={this.togglePersonsHandler}
+
+            />
+          {persons}
+      </div>
+    );
+
+      // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this work'))
+  }
+}
+
+export default App;
